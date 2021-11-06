07e25515fc20c49b769bade5b1389b6079883402
denis <dt@doctorbox.eu>
2021-11-06 15:32:24 +0100

    Basement for navigation.

--- app/src/main/java/com/denis/mvi_recyclerview_coil_hilt_espresso_junit/ui/main/MainFragment.kt
+++ app/src/main/java/com/denis/mvi_recyclerview_coil_hilt_espresso_junit/ui/main/MainFragment.kt
@@ -1,32 +1,13 @@
 package com.denis.mvi_recyclerview_coil_hilt_espresso_junit.ui.main
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.denis.mvi_recyclerview_coil_hilt_espresso_junit.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
+class MainFragment : Fragment(R.layout.main_fragment) {
 
     private val viewModel by viewModels<MainViewModel>()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
 
 }
