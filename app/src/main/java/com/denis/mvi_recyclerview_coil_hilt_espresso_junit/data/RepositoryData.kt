021eaf9027a224019dd541893f6fe24226961a29
denis <dt@doctorbox.eu>
2021-11-06 18:14:41 +0100

    As we have a possibility to get items one bu one, we can reduce traffic for the user with requesting only needed number of items. For this purposes I want to use Pagination. It helps me to postpone this work(control of updates and getting new items) to external library. This commit prepare network and presentation data models for upcoming conversion from one layer to another.

--- app/src/main/java/com/denis/mvi_recyclerview_coil_hilt_espresso_junit/data/RepositoryData.kt
+++ app/src/main/java/com/denis/mvi_recyclerview_coil_hilt_espresso_junit/data/RepositoryData.kt
@@ -1,5 +1,9 @@
 package com.denis.mvi_recyclerview_coil_hilt_espresso_junit.data
 
 data class RepositoryData(
-    val someValue: Boolean
+    val albumId: Int,
+    val id: Int,
+    val title: String,
+    val url: String,
+    val thumbnailUrl: String,
 )
