5d3de7d30d67d19f7d9ad54ba0381999be7b2ed6
denis <dt@doctorbox.eu>
2021-11-06 16:01:23 +0100

    Basement for navigation.

--- app/src/main/java/com/denis/mvi_recyclerview_coil_hilt_espresso_junit/MainActivity.kt
+++ app/src/main/java/com/denis/mvi_recyclerview_coil_hilt_espresso_junit/MainActivity.kt
@@ -2,13 +2,17 @@ package com.denis.mvi_recyclerview_coil_hilt_espresso_junit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.denis.mvi_recyclerview_coil_hilt_espresso_junit.databinding.MainActivityBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
